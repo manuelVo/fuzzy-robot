8f52d256bc360adbc20da31788621a081cffe74d
Manuel Vögele <develop@manuel-voegele.de>
2015-04-21 14:50:41 +0200

    Code reformatting

--- configuration.py
+++ configuration.py
@@ -10,61 +10,70 @@ INSTALLED_GAMES_FILE_NAME = "installed_games.json"
 cloudfolder = ""
 games = []
 
+
 def expand_path(path):
-	return os.path.expandvars(os.path.expanduser(path))
+    return os.path.expandvars(os.path.expanduser(path))
+
 
 if platform.system() == "Windows":
-	data_directory = "%LOCALAPPDATA%\\fuzzy-robot"
+    data_directory = "%LOCALAPPDATA%\\fuzzy-robot"
 else:
-	data_directory = "~/.fuzzy-robot"
+    data_directory = "~/.fuzzy-robot"
 
 data_directory = expand_path(data_directory)
 game_images_folder = os.path.join(data_directory, "images")
-	
-def exists ():
-	return os.path.exists(expand_path(os.path.join(data_directory, CONFIG_FILE_NAME)))
-	
-def load ():
-	global games
-	global filepath
-	global cloudfolder
-	filepath = expand_path(os.path.join(data_directory, CONFIG_FILE_NAME))
-	with open(filepath) as configuration_file:
-		config = json.load(configuration_file)
-	cloudfolder = expand_path(config["cloudfolder"])
-	games_file_path = os.path.join(data_directory, GAMES_FILE_NAME)
-	if os.path.exists(games_file_path):
-		with open(games_file_path) as games_file:
-			games = json.load(games_file)
-	else:
-		update_games()
 
-def save ():
-	try:
-		os.makedirs(data_directory)
-	except FileExistsError:
-		pass
-	data = {"cloudfolder":cloudfolder}
-	filepath = expand_path(os.path.join(data_directory, CONFIG_FILE_NAME))
-	with open(filepath, 'w') as configuration_file:
-		json.dump(data, configuration_file)
 
-def update_games ():
-	global games
-	try:
-		os.makedirs(game_images_folder)
-	except FileExistsError:
-		pass
-	jsonstr = urllib.request.urlopen("https://raw.githubusercontent.com/manuelVo/fuzzy-robot/master/games.json").read().decode("utf-8")
-	with open(os.path.join(data_directory, GAMES_FILE_NAME), "w") as games_file:
-		games_file.write(jsonstr)
-	games = json.loads(jsonstr)
-	for game in games:
-		image_file_name = os.path.join(game_images_folder, game["id"] + ".png")
-		if not os.path.exists(image_file_name):
-			try:
-				image = urllib.request.urlopen("https://raw.githubusercontent.com/manuelVo/fuzzy-robot/master/images/" + game["id"] + ".png").read()
-				with open(image_file_name, "wb") as image_file:
-					image_file.write(image)
-			except HTTPError:
-				pass
+def exists():
+    return os.path.exists(expand_path(os.path.join(data_directory, CONFIG_FILE_NAME)))
+
+
+def load():
+    global games
+    global filepath
+    global cloudfolder
+    filepath = expand_path(os.path.join(data_directory, CONFIG_FILE_NAME))
+    with open(filepath) as configuration_file:
+        config = json.load(configuration_file)
+    cloudfolder = expand_path(config["cloudfolder"])
+    games_file_path = os.path.join(data_directory, GAMES_FILE_NAME)
+    if os.path.exists(games_file_path):
+        with open(games_file_path) as games_file:
+            games = json.load(games_file)
+    else:
+        update_games()
+
+
+def save():
+    try:
+        os.makedirs(data_directory)
+    except FileExistsError:
+        pass
+    data = {"cloudfolder": cloudfolder}
+    filepath = expand_path(os.path.join(data_directory, CONFIG_FILE_NAME))
+    with open(filepath, 'w') as configuration_file:
+        json.dump(data, configuration_file)
+
+
+def update_games():
+    global games
+    try:
+        os.makedirs(game_images_folder)
+    except FileExistsError:
+        pass
+    jsonstr = urllib.request.urlopen(
+        "https://raw.githubusercontent.com/manuelVo/fuzzy-robot/master/games.json").read().decode("utf-8")
+    with open(os.path.join(data_directory, GAMES_FILE_NAME), "w") as games_file:
+        games_file.write(jsonstr)
+    games = json.loads(jsonstr)
+    for game in games:
+        image_file_name = os.path.join(game_images_folder, game["id"] + ".png")
+        if not os.path.exists(image_file_name):
+            try:
+                image = urllib.request.urlopen(
+                    "https://raw.githubusercontent.com/manuelVo/fuzzy-robot/master/images/" + game[
+                        "id"] + ".png").read()
+                with open(image_file_name, "wb") as image_file:
+                    image_file.write(image)
+            except HTTPError:
+                pass
--- main.py
+++ main.py
@@ -2,81 +2,96 @@ from tkinter import *
 from tkinter import filedialog
 from tkinter import messagebox
 from tkinter import ttk
-from PIL import Image, ImageTk
-import savesync
 import os.path
-import configuration as config
 import platform
+
+from PIL import Image, ImageTk
+
+import savesync
+
+import configuration as config
+
 
 root = Tk()
 root.title("Fuzzy Robot")
 mainframe = Frame(root)
-mainframe.grid(row=1, column=1, columnspan=2, sticky=(N,W,E,S))
+mainframe.grid(row=1, column=1, columnspan=2, sticky=(N, W, E, S))
 
 if platform.system() == "Linux":
-	ttk.Style().theme_use("alt")
+    ttk.Style().theme_use("alt")
 
-def browse_cloudfolder ( ):
-	newdir = filedialog.askdirectory()
-	if newdir != "":
-		global cfolder
-		cfolder.set(newdir)
-		
-def apply_cloudfolder ():
-	global cfolder
-	folder = cfolder.get()
-	if os.path.isdir(folder):
-		config.cloudfolder = folder
-		config.save()
-		folderpicker.withdraw()
-		draw_main_window()
-		root.deiconify()
-	else:
-		messagebox.showerror("Invalid folder", "The selected folder doesn't exist")
-		
-def draw_main_window ():
-	global mainframe
-	global games
-	mainframe.grid_forget()
-	mainframe = Frame(root)
-	mainframe.grid(row=1, column=1, columnspan=2, sticky=(N,W,E,S))
-	if not "games" in globals():
-		games = savesync.detect_games()
-	for game in games:
-		if savesync.is_synchronized(game):
-			state = 'disabled'
-		else:
-			state = 'normal'
-		if not "selected" in game:
-			game["selected"] = IntVar()
-			game["selected"].set(1)
-		try:
-			image = ImageTk.PhotoImage(Image.open(os.path.join(config.game_images_folder, game["id"] + ".png")).resize((32, 32), Image.ANTIALIAS))
-		except FileNotFoundError:
-			image = None
-		checkbutton = ttk.Checkbutton(mainframe, variable=game["selected"], image=image, text=game["name"], compound=LEFT, takefocus=False, state=state)
-		checkbutton.image = image
-		checkbutton.grid(sticky=(W,E))
-	pass
-	
-def synchronize ():
-	global games
-	syncgames = [game for game in games if game["selected"].get() == 1 and not savesync.is_synchronized(game)]
-	for game in syncgames:
-		if savesync.has_conflicts(game):
-			if messagebox.askyesno("Savegame conflict", "Game '" + game["name"] + "' has local saves as well as synchronized saves. Do you want to overwrite the local saves with the synchronized saves?\n\n(No cancels synchronization for this game)"):
-				savesync.remove_local_savegame(game)
-			else:
-				continue
-		savesync.move_save_to_cloud(game)
-	messagebox.showinfo("Synchronization done", "The selected games are now set up for synchronization and will store their saves in the specified folder.")
-	draw_main_window()
+
+def browse_cloudfolder():
+    newdir = filedialog.askdirectory()
+    if newdir != "":
+        global cfolder
+        cfolder.set(newdir)
+
+
+def apply_cloudfolder():
+    global cfolder
+    folder = cfolder.get()
+    if os.path.isdir(folder):
+        config.cloudfolder = folder
+        config.save()
+        folderpicker.withdraw()
+        draw_main_window()
+        root.deiconify()
+    else:
+        messagebox.showerror("Invalid folder", "The selected folder doesn't exist")
+
+
+def draw_main_window():
+    global mainframe
+    global games
+    mainframe.grid_forget()
+    mainframe = Frame(root)
+    mainframe.grid(row=1, column=1, columnspan=2, sticky=(N, W, E, S))
+    if not "games" in globals():
+        games = savesync.detect_games()
+    for game in games:
+        if savesync.is_synchronized(game):
+            state = 'disabled'
+        else:
+            state = 'normal'
+        if not "selected" in game:
+            game["selected"] = IntVar()
+            game["selected"].set(1)
+        try:
+            image = ImageTk.PhotoImage(
+                Image.open(os.path.join(config.game_images_folder, game["id"] + ".png")).resize((32, 32),
+                                                                                                Image.ANTIALIAS))
+        except FileNotFoundError:
+            image = None
+        checkbutton = ttk.Checkbutton(mainframe, variable=game["selected"], image=image, text=game["name"],
+                                      compound=LEFT, takefocus=False, state=state)
+        checkbutton.image = image
+        checkbutton.grid(sticky=(W, E))
+    pass
+
+
+def synchronize():
+    global games
+    syncgames = [game for game in games if game["selected"].get() == 1 and not savesync.is_synchronized(game)]
+    for game in syncgames:
+        if savesync.has_conflicts(game):
+            if messagebox.askyesno("Savegame conflict", "Game '" + game[
+                "name"] + "' has local saves as well as synchronized saves. Do you want to overwrite the local saves with the synchronized saves?\n\n(No cancels synchronization for this game)"):
+                savesync.remove_local_savegame(game)
+            else:
+                continue
+        savesync.move_save_to_cloud(game)
+    messagebox.showinfo("Synchronization done",
+                        "The selected games are now set up for synchronization and will store their saves in the specified folder.")
+    draw_main_window()
+
 
 def update():
-	global games
-	config.update_games()
-	games = savesync.detect_games()
-	draw_main_window()
+    global games
+    config.update_games()
+    games = savesync.detect_games()
+    draw_main_window()
+
 
 Button(root, text="Synchronize selected", command=synchronize).grid(row=2, column=1, sticky=W)
 Button(root, text="Update supported game list", command=update).grid(row=2, column=2, sticky=E)
@@ -84,16 +99,18 @@ Button(root, text="Update supported game list", command=update).grid(row=2, colu
 cfolder = StringVar()
 folderpicker = Toplevel()
 folderpicker.title("Setup sync folder")
-ttk.Label(folderpicker, text="Please select the folder to which you want your saves synchronized").grid(row=1, column=1, columnspan=2, sticky=(W,E))
+ttk.Label(folderpicker, text="Please select the folder to which you want your saves synchronized").grid(row=1, column=1,
+                                                                                                        columnspan=2,
+                                                                                                        sticky=(W, E))
 ttk.Entry(folderpicker, exportselection=0, textvariable=cfolder).grid(row=2, column=1, sticky=(E, W))
 ttk.Button(folderpicker, text="Browse...", command=browse_cloudfolder).grid(row=2, column=2, sticky=(W))
 ttk.Button(folderpicker, text="Ok", command=apply_cloudfolder).grid(row=3, column=2, sticky=(E))
-		
+
 if config.exists():
-	folderpicker.withdraw()
-	config.load()
-	draw_main_window()
+    folderpicker.withdraw()
+    config.load()
+    draw_main_window()
 else:
-	root.withdraw()
+    root.withdraw()
 
 root.mainloop()
